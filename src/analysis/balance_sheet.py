0de8a0d4aa354e6c6b9b760f8dfe5f858061fc11
alan191006 <hdmquan@outlook.com>
2024-12-16 18:33:29 +1100

    refactor: Update prompting implementation
    - Replaced openAIClient with send_prompt function for better abstraction.
    - Improved prompt handling within the analyse function.
    - Adjusted temperature parameter for the send_prompt function to fit context of financial analysis 0.7 -> 0.3
    - Added error logging for unsupported models in send_prompt.

--- src/analysis/balance_sheet.py
+++ src/analysis/balance_sheet.py
@@ -1,20 +1,20 @@
 import os
 import pandas as pd
 from loguru import logger
-from utils import PATH, openAIClient
+from utils import PATH, send_prompt
 
 file_path = PATH.data_processed / "Balance Sheet.csv"
 
 # TODO: Revise and improve this prompt
 # Current output is very undesirable
-prompt = """ 
-    {balance_sheet}
-    
-    Analyze the balance sheet of this real estate company to assess its financial health and performance. Identify key financial ratios and trends to provide a comprehensive overview of its financial position. Your analysis should be as concise as possible without compromising any information and do not make up numbers and things.
-    """
 
 
 def analyse():
+    prompt = """ 
+        {balance_sheet}
+        
+        Analyze the balance sheet of this real estate company to assess its financial health and performance. Identify key financial ratios and trends to provide a comprehensive overview of its financial position. Your analysis should be as concise as possible without compromising any information and do not make up numbers and things.
+    """
 
     if not os.path.exists(file_path):
         logger.error(f"File {file_path.name} does not exist")
@@ -23,18 +23,8 @@ def analyse():
     # Balance sheet is small enough for the LLM to directly analyse
     with open(file_path) as f:
         balance_sheet = f.read().strip()
-        client = openAIClient()
-
-        # TODO: Fine-tune parameters
-        response = client.chat.completions.create(
-            model="gpt-4o",
-            temperature=0.7,
-            messages=[
-                {"role": "user", "content": prompt.format(balance_sheet=balance_sheet)}
-            ],
-        )
-
-        return response.choices[0].message.content
+        prompt = prompt.format(balance_sheet=balance_sheet)
+        return send_prompt(prompt)
 
 
 if __name__ == "__main__":
--- src/analysis/utils.py
+++ src/analysis/utils.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from openai import OpenAI
 from io import StringIO
+from loguru import logger
 
 
 class PATH:
@@ -12,9 +13,20 @@ class PATH:
 
 
 # TODO: Add multi model interface support
-def openAIClient():
-    api_key = open(PATH.src / "openai-api-key.txt").read().strip()
-    return OpenAI(api_key=api_key)
+def send_prompt(prompt, model: str = "gpt-4o", temperature=0.3):
+    if model in ["gpt-4o", "gpt-4o-mini"]:
+        api_key = open(PATH.src / "openai-api-key.txt").read().strip()
+        client = OpenAI(api_key=api_key)
+
+        response = client.chat.completions.create(
+            model=model,
+            temperature=temperature,
+            messages=[{"role": "user", "content": prompt}],
+        )
+
+        return response.choices[0].message.content
+    else:
+        logger.error(f"Model {model} not found or supported")
 
 
 def dataframe_to_csv_text(df):
