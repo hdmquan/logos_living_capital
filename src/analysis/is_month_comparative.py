b046325e7e423a2e5032afaa900812c233ac4ee6
Alan Huynh <hdmquan@outlook.com>
2024-12-25 16:47:43 +1100

    fix: Data integrity
    - Edit the script to output exact to what the table show in the xlsx file

--- src/analysis/is_month_comparative.py
+++ src/analysis/is_month_comparative.py
@@ -9,7 +9,7 @@ except (ModuleNotFoundError, ImportError):
     from .utils import PATH, send_prompt, df_to_csv_text
     from .prompt import Prompt
 
-file_name = "Revenue Detailed.csv"
+file_name = "IS Month Comparative Detailed.csv"
 
 
 def analyse(file_dir=PATH.data_processed):
@@ -36,25 +36,35 @@ def get_data(file_dir=PATH.data_processed):
     # Read the income statement into a DataFrame
     data = pd.read_csv(file_path, index_col=0)
 
+    # print(data.index.tolist())  # Log the row names
+    data = data.loc["Nursing Expenses":"Total Real Estate Taxes"]
+    data = data[~data.index.str.contains("total", case=False)]
+
     # data = data.loc["Total Revenue":"Operating Expenses"]
     # data = data.drop(index=["Total Revenue", "Operating Expenses"])
 
-    data = data.iloc[:, [-4, -2, -1]]
+    data = data.iloc[:, [-2, -1]]
 
+    data.iloc[:, -1] = data.iloc[:, -1] * 100
+
+    
     percent_var_top = data.sort_values(by=data.columns[-1], ascending=False).head(10)
-    percent_var_top.columns = ["Percent Variance (%)", "% Value", "$ Value"]
-
-    # percent_var_top.iloc[-1] = (
-    #     percent_var_top.iloc[-1].astype(float).round(2).astype(str) + "%"
-    # )
-
-    dollar_var_top = data.sort_values(by=data.columns[-2], ascending=False).head(10)
-    dollar_var_top.columns = ["Dollar ($) Variance", "% Value", "$ Value"]
-    dollar_var_top.insert(0, "Rank", range(1, 1 + len(dollar_var_top)))
+    percent_var_top.columns = ["% Value", "$ Value"]
     percent_var_top.insert(0, "Rank", range(1, 1 + len(percent_var_top)))
 
-    return dollar_var_top, percent_var_top
+
+    dollar_var_top = data.sort_values(by=data.columns[-2], ascending=False).head(10)
+    dollar_var_top.columns = ["% Value", "$ Value"]
+    dollar_var_top.insert(0, "Rank", range(1, 1 + len(dollar_var_top)))
+    dollar_var_top = dollar_var_top.reindex(columns=["Rank", "% Value", "$ Value"])
+    
+
+    return dollar_var_top.round().astype(int), percent_var_top.round().astype(int)
 
 
 if __name__ == "__main__":
-    logger.info(response := analyse())
+    # logger.info(response := analyse())
+    a, b = get_data()
+    print(a)
+    print()
+    print(b)
